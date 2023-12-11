377586477b467e319cc99544dc5c844a9781acbf
OleksandrD <oleksandr.d@expletech.com>
2023-12-11 11:24:41 +0200

    fix auth

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,26 +4,16 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { IJwtPayload } from '../../common/interfaces/auth.interface';
 
-import { Request } from 'express';
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        JwtStrategy.ExtractJWT,
         ExtractJwt.fromAuthHeaderAsBearerToken(),
       ]),
       ignoreExpiration: false,
       secretOrKey: configService.get('APP_JWT_SECRET'),
     });
-  }
-
-  private static ExtractJWT(req: Request) {
-    if (req.cookies) {
-      console.log(req.cookies['refresh']);
-      return req.cookies['refresh'];
-    }
   }
 
   async validate(payload: IJwtPayload) {
