c6d09f2dcc94e38032dbefaf868cdd6dbd4638d7
OleksandrD <oleksandr.d@expletech.com>
2023-12-12 11:24:04 +0200

    add new entities BlockList UserSetting, update UserEntity and create Migration for this change.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { BlockList } from './entities/block-list.entity';
+import { UserSetting } from './entities/user-setting.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, BlockList, UserSetting])],
   controllers: [UsersController],
   providers: [UsersService],
 })
